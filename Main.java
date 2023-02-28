815dc1236f13b3d1079aa0451b17d1f0403974d5
tehepix2 <101379979+tehepix2@users.noreply.github.com>
2023-02-28 14:46:32 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+        lib.clearConsole();
+
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> songList = new ArrayList<String>();
+        System.out.println("Spitofy");
+        
+        
+    } 
+}
