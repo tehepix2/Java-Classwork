f78294f53ad4936fd07c4b30edf96a674eda5bdf
tehepix2 <101379979+tehepix2@users.noreply.github.com>
2023-03-08 14:50:05 -0500

    Added more features to Main

--- Main.java
+++ Main.java
@@ -21,14 +21,50 @@ class Main {
                     lib.clearConsole();
                 }
                 else {
-                    for (Song i:songList) {
-                        System.out.println(i.getName());
+                    boolean songLoop = true;
+                    while (songLoop) {
+                        int listCounter = 1;
+                        System.out.println("Choose the song you want to view.");
+                        System.out.println("Your songs: ");
+                        System.out.println("0 -- Go Back");
+                        
+                        for (Song i:songList) {
+                            System.out.println(listCounter + " -- " + i.getName());
+                            listCounter++;
+                        }
+                        int songChoice = input.nextInt();
+                        boolean songViewLoop = true;
+                        if (songChoice == 0) {
+                            songLoop = false;
+                        }
+                        else if (songChoice != 0) {
+                            while (songViewLoop) {
+                                
+                            }
+                        }
+
                     }
+
                 }
             }
             else if(userChoice == 2) {
                 
                 System.out.print("Song name: ");
+                String songNameChoice = input.next();
+                lib.clearConsole();
+                System.out.print("Song cost: ");
+                double songCostChoice = input.nextDouble();
+                lib.clearConsole();
+                System.out.print("Your rating (0 - 10): ");
+                double songRatingChoice = input.nextDouble();
+                lib.clearConsole();
+                System.out.print("Song author: ");
+                String songAuthorChoice = input.next();
+                lib.clearConsole();
+                songList.add(new Song(songNameChoice, songCostChoice, songRatingChoice, songAuthorChoice));
+                System.out.print("Song added!");
+                lib.sleep(2000);
+                lib.clearConsole();
 
             }
         }
