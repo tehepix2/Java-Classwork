f180395715e15c1320d849d25091a90e5432e442
tehepix2 <101379979+tehepix2@users.noreply.github.com>
2023-03-10 14:23:00 +0000

    users can now view and edit atrributes of songs

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ class Main {
         
         while (true) {
             System.out.println("Spitofy");
-            System.out.println("What would you like to do?\n1 -- Song List\n2 -- Add a new song\n3 -- Most expensive song\n4 -- Cheapest Song\n5 -- Search songs by an artist\n6 -- Search earliest release\n7 -- Search latest release");
+            System.out.println("What would you like to do?\n1 -- Song List\n2 -- Add a new song\n3 -- Most expensive song\n4 -- Cheapest Song\n5 -- Search songs by an artist\n6 -- Search earliest release\n7 -- Search latest release\n8 -- Leave Program");
             int userChoice = input.nextInt();
             lib.clearConsole();
             if (userChoice == 1) {
@@ -33,6 +33,7 @@ class Main {
                             listCounter++;
                         }
                         int songChoice = input.nextInt();
+                        lib.clearConsole();
                         boolean songViewLoop = true;
                         if (songChoice == 0) {
                             songLoop = false;
@@ -40,17 +41,80 @@ class Main {
                         else {
                             while (songViewLoop) {
                                 System.out.println("What would you like to do?");
-                                System.out.println("Song name: " + songList(songChoice).songName);
-                                System.out.println("Author: " + ongList(songChoice).songAuthor);
-                                System.out.println("Your rating: " + songList(songChoice).songRating);
-                                System.out.println("Release date: " + songList(songChoice).songMonth + "/" + songList(songChoice).songDay + "/" + songList(songChoice).songYear);
-                                System.out.println("0 -- Go Back\n1 -- Edit Name\n2 -- Edit Rating\n3 -- Edit Author\n4 -- Edit Song Cost\n5 -- Edit Release Date");
-
+                                System.out.println("Song name: " + songList.get(songChoice - 1).getName());
+                                System.out.println("Author: " + songList.get(songChoice - 1).getAuthorF() + " " + songList.get(songChoice - 1).getAuthorL());
+                                System.out.println("Price: $" + songList.get(songChoice - 1).getCost());
+                                System.out.println("Your rating: " + songList.get(songChoice - 1).getRating());
+                                System.out.println("Release date: " + songList.get(songChoice - 1).getMonth() + "/" + songList.get(songChoice - 1).getDay() + "/" + songList.get(songChoice - 1).getYear());
+                                System.out.println("0 -- Go Back\n1 -- Edit Song Name\n2 -- Edit Rating\n3 -- Edit Author Name\n4 -- Edit Song Cost\n5 -- Edit Release Date");
+                                int songViewChoice = input.nextInt();
+                                lib.clearConsole();
+                                if (songViewChoice == 0) {
+                                    songViewLoop = false;
+                                }
+                                else if (songViewChoice == 1) {
+                                    System.out.print("Enter a new song name: ");
+                                    String newSongName = input.next();
+                                    lib.clearConsole();
+                                    songList.get(songChoice - 1).newName(newSongName);
+                                    System.out.println("Name successfully changed!");
+                                    lib.sleep(2000);
+                                    lib.clearConsole();
+                                }
+                                else if (songViewChoice == 2) {
+                                    System.out.print("Enter a new rating: ");
+                                    double newSongRating = input.nextDouble();
+                                    lib.clearConsole();
+                                    songList.get(songChoice - 1).newRating(newSongRating);
+                                    System.out.println("Rating successfully changed!");
+                                    lib.sleep(2000);
+                                    lib.clearConsole();
+                                }
+                                else if (songViewChoice == 3) {
+                                    System.out.print("Enter a new first name: ");
+                                    String newSongFName = input.next();
+                                    lib.clearConsole();
+                                    System.out.print("Enter a new last name: ");
+                                    String newSongLName = input.next();
+                                    lib.clearConsole();
+                                    songList.get(songChoice - 1).newAuthorFN(newSongFName);
+                                    songList.get(songChoice - 1).newAuthorLN(newSongLName);
+                                    System.out.println("Name successfully changed!");
+                                    lib.sleep(2000);
+                                    lib.clearConsole();
+                                }
+                                else if (songViewChoice == 4) {
+                                    System.out.print("Enter a new price: ");
+                                    double newSongPrice = input.nextDouble();
+                                    lib.clearConsole();
+                                    songList.get(songChoice - 1).newCost(newSongPrice);
+                                    System.out.println("Price successfully changed!");
+                                    lib.sleep(2000);
+                                    lib.clearConsole();
+                                }
+                                else if (songViewChoice == 5){
+                                    System.out.println("Enter a new day: ");
+                                    int newSongDay = input.nextInt();
+                                    lib.clearConsole();
+                                    System.out.println("Enter a new month: ");
+                                    int newSongMonth = input.nextInt();
+                                    lib.clearConsole();
+                                    System.out.println("Enter a new year: ");
+                                    int newSongYear = input.nextInt();
+                                    lib.clearConsole();
+                                    songList.get(songChoice - 1).newDate(newSongDay, newSongMonth, newSongYear);
+                                    System.out.println("Date successfully changed!");
+                                    lib.sleep(2000);
+                                    lib.clearConsole();
+                                }
+                                else {
+                                    System.out.println("Invalid input!");
+                                    lib.sleep(2000);
+                                    lib.clearConsole();
+                                }
                             }
                         }
-
                     }
-
                 }
             }
             else if(userChoice == 2) {
@@ -64,8 +128,11 @@ class Main {
                 System.out.print("Your rating (0 - 10): ");
                 double songRatingChoice = input.nextDouble();
                 lib.clearConsole();
-                System.out.print("Song author: ");
-                String songAuthorChoice = input.next();
+                System.out.print("Author's first name: ");
+                String songAuthorFChoice = input.next();
+                lib.clearConsole();
+                System.out.print("Author's last name: ");
+                String songAuthorLChoice = input.next();
                 lib.clearConsole();
                 System.out.println("Release date");
                 System.out.print("Day: ");
@@ -79,7 +146,7 @@ class Main {
                 System.out.print("Year: ");
                 int songYearChoice = input.nextInt();
                 lib.clearConsole();
-                songList.add(new Song(songNameChoice, songCostChoice, songRatingChoice, songAuthorChoice, songDayChoice, songMonthChoice, songYearChoice));
+                songList.add(new Song(songNameChoice, songCostChoice, songRatingChoice, songAuthorFChoice, songAuthorLChoice, songDayChoice, songMonthChoice, songYearChoice));
                 System.out.print("Song added!");
                 lib.sleep(2000);
                 lib.clearConsole();
