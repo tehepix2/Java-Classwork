e97ba67aa311eeaa6956a45692a2cc00b3fe55cf
tehepix2 <101379979+tehepix2@users.noreply.github.com>
2023-04-03 14:14:39 -0400

    Recursion lab finished

--- Main.java
+++ Main.java
@@ -1,8 +1,32 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        lib.clearConsole();
+        Scanner input = new Scanner(System.in);
+
+        boolean programLoop = true;
+        while (programLoop) {
+            System.out.print("Enter a number: ");
+            double numChoice = input.nextDouble();
+            lib.clearConsole();
+            System.out.print("Enter its exponent: ");
+            int powChoice = input.nextInt();
+            lib.clearConsole();
+            System.out.print(exp(numChoice, powChoice));
+            lib.sleep(3000);
+            lib.clearConsole();
+        }
     } 
+
+    public static double exp(double number, int power) {
+        if (power == 0) {
+            return 1;
+        }
+        else {
+            return number * exp(number, power - 1);
+        }
+    }
+    
 }
