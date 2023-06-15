f249ef93cf0096f88f1895e4783dcf89f900e5e4
tehepix2 <101379979+tehepix2@users.noreply.github.com>
2023-06-15 14:05:36 -0400

    actually finished

--- Main.java
+++ Main.java
@@ -3,25 +3,34 @@ import java.util.ArrayList;
 
 class Main {
     public static void main(String[] args) {
-        lib.clearConsole();
-        Scanner input = new Scanner(System.in);
-        System.out.println("How many numbers will you enter?");
-        System.out.print("Enter a number: ");
-        int numNums = input.nextInt();
-        int[] numArray = new int[numNums];
-        lib.clearConsole();
+        boolean restart = true;
+        while (restart) {
         
-        for (int i = 0; i < numNums; i++) {
-            System.out.println("Numbers entered: " + i + "/" + numArray.length);
-            System.out.print("Enter a number: ");
-            int numChoice = input.nextInt();
-            numArray[i] = numChoice;
             lib.clearConsole();
+            Scanner input = new Scanner(System.in);
+            System.out.println("How many numbers will you enter?");
+            System.out.print("Enter a number: ");
+            int numNums = input.nextInt();
+            int[] numArray = new int[numNums];
+            lib.clearConsole();
+        
+            for (int i = 0; i < numNums; i++) {
+                System.out.println("Numbers entered: " + i + "/" + numArray.length);
+                System.out.print("Enter a number: ");
+                int numChoice = input.nextInt();
+                numArray[i] = numChoice;
+                lib.clearConsole();
+            }
+        
+            System.out.println("All prime numbers:");
+            printArray(PrimeDirective(numArray));
+            System.out.print("Would you like to try again?\n1 -- Yes\n2 -- No\nEnter a number: ");
+            int restartChoice = input.nextInt();
+            lib.clearConsole();
+            if (restartChoice == 2) {
+                restart = false;
+            }
         }
-        
-        System.out.println("All prime numbers:");
-        printArray(PrimeDirective(numArray));
-        
     } 
 
     public static void printArray(int[] array) {
@@ -52,15 +61,12 @@ class Main {
         for (int i:array) {
             for (int k = 1; k < i + 1; k++) {
                 if (i%k == 0) {
-                    numFactors++;
-                    
+                    numFactors++;     
                 }
-                
             }
             if (numFactors <= 2) {
                 primeNumbers[arrayCounter] = i;
-                arrayCounter++;
-                
+                arrayCounter++; 
             }
             numFactors = 0;
         }
