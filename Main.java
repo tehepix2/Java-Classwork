bf297b5e0bf1c142e1dcdbf78f9d5a0d1cf620bb
tehepix2 <daniel12landa@gmail.com>
2023-09-11 09:24:40 -0400

    added stuff

--- Main.java
+++ Main.java
@@ -1,8 +1,25 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int M = 1000;
+       int D = 500;
+       int C = 100;
+       int L = 50;
+       int X = 10;
+       int V = 5;
+       int I = 1;
+       
+       
+        Scanner input = new Scanner(System.in);
+       lib.clearConsole();
+       lib.print("Enter a number: ");
+       int num = input.nextInt();
+       lib.clearConsole();
+
+
+
     } 
 }
