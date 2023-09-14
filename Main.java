c3fc3cb3ec8c76c5628a6e8007413344bfd2f9d7
tehepix2 <daniel12landa@gmail.com>
2023-09-13 22:24:59 -0400

    finished lab

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+
 
 class Main {
     public static void main(String[] args) {
@@ -22,23 +22,24 @@ class Main {
         lib.clearConsole();
 
         Scanner input = new Scanner(System.in);
-        System.out.print("What would you like to do?\n1 -- Roman to Number\n2 -- Number to Roman");
+        System.out.print("What would you like to do?\n1 -- Number to Roman\n2 -- Roman to Number\n");
         int menuChoice = input.nextInt();
         lib.clearConsole();
-        /*if (menuChoice == 1) {
+        if (menuChoice == 1) {
             System.out.print("Enter a number (1 - 3999): ");
             int num = input.nextInt();
             lib.clearConsole();
             getNumeral(num, values);
         }
+        
         else if (menuChoice == 2) {
             System.out.print("Enter a numeral (1 - 3999): ");
-            String numeral = input.nextLine();
+            String numeral = input.next();
             lib.clearConsole();
             getNumber(numeral, values);
-        } */
+        } 
         
-        String test = "You";
+        
         
         
         
@@ -51,13 +52,97 @@ class Main {
         for(int i = 0; i < num.length(); i++) {
             switch(num.charAt(i)) {
                 case ('M'):
-                number+=1000;
-                case ('CM'):
-                number
+                if (i == 0) {
+                    number+=1000;
+                    
+                }
+                else {
+                    if (num.charAt(i - 1) == 'C') {
+                        number +=800;
+                    }
+                    else {
+                        number += 1000;
+                    }
+                }
+                break;
+                
+                case ('C'):
+                if (i == 0) {
+                    number+=100;
+                    
+                }
+                else {
+                    if (num.charAt(i - 1) == 'X') {
+                        number +=80;
+                    }
+                    else {
+                        number +=100;
+                    }
+                }
+                break;
+
+                case('D'):
+                if (i == 0) {
+                    number+=500;
+                    
+                }
+                else {
+                    if (num.charAt(i - 1) == 'C') {
+                        number +=300;
+                    }
+                    else {
+                        number +=500;
+                    }
+                }
+                break;
+                case('X'):
+                if (i == 0) {
+                    number+=10;
+                    
+                }
+                else {
+                    if (num.charAt(i - 1) == 'I') {
+                        number +=8;
+                    }
+                    else {
+                        number +=10;
+                    }
+                }
+                break;
+                case('L'):
+                if (i == 0) {
+                    number+=50;
+                    
+                }
+                else {
+                    if (num.charAt(i - 1) == 'X') {
+                        number +=30;
+                    }
+                    else {
+                        number += 50;
+                    }
+                }
+                break;
+                case('V'):
+                if (i == 0) {
+                    number+=5;
+                    
+                }
+                else {
+                    if (num.charAt(i - 1) == 'I') {
+                        number +=3;
+                    }
+                }
+                break;
+                case('I'):
+                number +=1;
+
+                
                 
 
             }
         }
+        System.out.println(number);
     }
     public static void getNumeral(int num, Roman[] list) {
         
