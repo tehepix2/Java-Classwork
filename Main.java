38b94476146a394490b366a639ecc88c60f81c36
tehepix2 <101379979+tehepix2@users.noreply.github.com>
2023-09-15 08:55:06 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,20 +4,7 @@ import java.util.ArrayList;
 
 class Main {
     public static void main(String[] args) {
-       int M = 1000;
-       int D = 500;
-       int C = 100;
-       int L = 50;
-       int X = 10;
-       int V = 5;
-       int I = 1;
-       
-       
-        Scanner input = new Scanner(System.in);
-       lib.clearConsole();
-       lib.print("Enter a number: ");
-       int num = input.nextInt();
-       lib.clearConsole();
+      System.out.println("Hello World!");
 
 
 
