120ab9a2b6c10cfee082f07de5f7c806d1e53368
clandaverde <daniel12landa@gmail.com>
2024-02-08 09:23:08 -0500

    started lab

--- Main.java
+++ Main.java
@@ -1,12 +1,31 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.lang.Math;
 
 
 class Main {
     public static void main(String[] args) {
-      System.out.println("Hello World!");
-
-
-
+      lib.clearConsole();
+      File myFile = new File("Stuff.txt");
+      try {
+        if (myFile.createNewFile()) {
+          System.out.println("File created: " + myFile.getName());
+        }
+        else {
+          System.out.println("File already exists.");
+          myFile.delete();
+        }
+      }
+      catch(IOException e) {
+        System.out.println("Error.");
+        e.printStackTrace();
+      }
+      
     } 
+
+    /*public static double distanceCalc(double x, double y, double z) {
+      sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2);
+    }*/
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Java stuff
+You can learn about Java File I/O from online tutorials, such as W3schools (W3Schools.com).  Go to the Java tutorial to learn about how Java handles file I/O.
+
+Create a Java program that computes the difference between two sets of (x,y,z) coordinates. The program should read two coordinates and compute their distance, and write that distance into an output file.  Then, the computer  should read the next two sets of coordinates and write their distance into the output file.  And so on, until we get to the end of the file.
+
+The program should use an input file if it already exists, allow the user to create one and enter (x, y, z) points if the file does not already exist.  The computer should read two sets of points, and calculate their distance according to the distance formula.
