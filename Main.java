6fc3584112a7215bf7268924dbbfb70c57833c98
clandaverde <daniel12landa@gmail.com>
2024-03-07 09:22:53 -0500

    stuff

--- Main.java
+++ Main.java
@@ -13,61 +13,67 @@ public class Main {
     public static void main(String[] args) throws IOException {
         lib.clearConsole();
         File myCoords = new File("Coordinates.txt");
-        
-        Scanner input = new Scanner(System.in);
-        List<Integer> coordinates = new ArrayList<Integer>();
-        
-        System.out.print("Enter an X coordinate: ");
-        String xcoord = input.next();
-        writeCoords(myCoords, xcoord);
-        lib.clearConsole();
-        
-        
-        System.out.print("Enter a Y coordinate: ");
-        String ycoord = input.next();
-        writeCoords(myCoords, ycoord);
-        lib.clearConsole();
-        
-        
-        System.out.print("Enter a Z coordinate: ");
-        String zcoord = input.next();
-        writeCoords(myCoords, zcoord);
-        lib.clearConsole(); 
+        boolean programLoop = true;
+        while (programLoop) {
+          Scanner input = new Scanner(System.in);
+          List<Integer> coordinates = new ArrayList<Integer>();
+          
+          System.out.print("Enter an X coordinate: ");
+          String xcoord = input.next();
+          writeCoords(myCoords, xcoord);
+          lib.clearConsole();
+          
+          
+          System.out.print("Enter a Y coordinate: ");
+          String ycoord = input.next();
+          writeCoords(myCoords, ycoord);
+          lib.clearConsole();
+          
+          
+          System.out.print("Enter a Z coordinate: ");
+          String zcoord = input.next();
+          writeCoords(myCoords, zcoord);
+          lib.clearConsole(); 
 
-        System.out.print("Enter an X coordinate: ");
-        xcoord = input.next();
-        writeCoords(myCoords, xcoord);
-        lib.clearConsole();
-        
-        
-       System.out.print("Enter a Y coordinate: ");
-        ycoord = input.next();
-        writeCoords(myCoords, ycoord);
-        lib.clearConsole();
-        
-        
-        System.out.print("Enter a Z coordinate: ");
-        zcoord = input.next();
-        writeCoords(myCoords, zcoord);
-        lib.clearConsole(); 
-        
-        
-        Scanner scanner = new Scanner(myCoords);
-        scanner.useDelimiter(" ");
-        
-        while (scanner.hasNext()) {
-            String next = scanner.next();
-            coordinates.add(Integer.valueOf(next));
-        }
-        
-        scanner.close();
-        
-        for (Integer i : coordinates) {
-          System.out.println(i.intValue());
-        }
-        myCoords.delete();
-       
+          System.out.print("Enter an X coordinate: ");
+          xcoord = input.next();
+          writeCoords(myCoords, xcoord);
+          lib.clearConsole();
+          
+          
+        System.out.print("Enter a Y coordinate: ");
+          ycoord = input.next();
+          writeCoords(myCoords, ycoord);
+          lib.clearConsole();
+          
+          
+          System.out.print("Enter a Z coordinate: ");
+          zcoord = input.next();
+          writeCoords(myCoords, zcoord);
+          lib.clearConsole(); 
+          
+          
+          Scanner scanner = new Scanner(myCoords);
+          scanner.useDelimiter(" ");
+          
+          while (scanner.hasNext()) {
+              String next = scanner.next();
+              coordinates.add(Integer.valueOf(next));
+          }
+          
+          
+          
+          for (Integer i : coordinates) {
+            System.out.println(i.intValue());
+          }
+          myCoords.delete();
+          
+          
+          writeCoords(myCoords, "" + distanceCalc(coordinates.get(0).intValue(), coordinates.get(1).intValue(), coordinates.get(2).intValue(), coordinates.get(3).intValue(), coordinates.get(4).intValue(), coordinates.get(5).intValue()));
+          scanner.close();
     }
+        }
+        
     public static double distanceCalc(double x, double x1, double y, double y1, double z, double z1) {
       return Math.sqrt(Math.pow((x - x1), 2) + Math.pow((y - y1), 2) + Math.pow((z - z1), 2));
     }
